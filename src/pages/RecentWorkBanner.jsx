feb28273ca31c5c9370cd9c5e60b8424ed4f8f59
akash100a <arakash968786@gmail.com>
2025-05-25 11:12:09 +0600

    Implement RecentWorkBanner component and context for managing recent works

--- src/pages/RecentWorkBanner.jsx
+++ src/pages/RecentWorkBanner.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const RecentWorkBanner = () => {
   return (
-    <div>RecentWorkBanner</div>
+   <>
+      <div className="flex gap-4 p-4">
+        <div className="w-1/3">
+          {/* <RecentWorkCreateForm /> */}
+        </div>
+
+        <div className="w-2/3">
+          {/* <RecentWorksList /> */}
+        </div>
+      </div>
+    </>
   )
 }
 
