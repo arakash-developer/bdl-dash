73eecb2226ccb0af6095445dd1f65ba4abc72031
akash100a <arakash968786@gmail.com>
2025-06-02 21:11:45 +0600

    Remove stray characters from RecentWorkBanner component

--- src/components/MokupBanner.jsx
+++ src/components/MokupBanner.jsx
@@ -5,7 +5,6 @@ const RecentWorkBanner = () => {
     <>
       <div className="flex gap-4 p-4">
         <div className="w-1/3">{/* <RecentWorkBannerForm /> */}</div>
-ss
         <div className="w-2/3">{/* <RecentWorkBannerList /> */}</div>
       </div>
     </>
